30d5323476948a443f22424fe581a78f64973978
benayaram rekha <96018533+benayaram@users.noreply.github.com>
2025-08-14 12:57:13 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,17 +1,30 @@
 print("--- Welcome to Calculator ---")
-print("Enter a full calculation (e.g., 5 * (10 + 2) / 3)")
+print("Enter a calculation using numbers and operators (+, -, *, /).")
 print("Type 'exit' to quit.")
 print("-" * 30)
+
+ALLOWED_CHARS = "0123456789.+-*/() "
 
 while True:
     expression = input("Calculate: ")
     if expression.lower() == 'exit':
         break
-    try:
-        result = eval(expression)
-        print(f"Result: {result}")
-    except Exception as e:
-        print(f"Error: Invalid expression. \nDetails: {e}")
-    print("-" * 30) 
+
+    is_valid = True 
+    for char in expression:
+
+        if char not in ALLOWED_CHARS:
+            is_valid = False
+            break
+    if is_valid:
+        try:
+            result = eval(expression)
+            print(f"Result: {result}")
+        except Exception as e:
+            print(f"Error: Invalid expression. \nDetails: {e}")
+    else:
+        print(f"Error: Invalid characters found. Please only use numbers and math operators.")
+
+    print("-" * 30)
 
 print("--- Calculator closed! ---")
